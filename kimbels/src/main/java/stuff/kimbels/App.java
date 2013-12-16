49122aee69e917fa3d7cd7ebd13425227c683ef9
scontro <hilla.fred@helsinki.fi>
2013-12-16 20:32:36 +0200

    nb-projekti lisätty

---
+++ kimbels/src/main/java/stuff/kimbels/App.java
@@ -0,0 +1,13 @@
+package stuff.kimbels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
